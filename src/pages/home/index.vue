841e9d9b25945614e74afbe6ee3475f65978598e
camusama <a1012401749@163.com>
2018-08-18 14:58:21 +0800

    1

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,44 +4,91 @@
     <div class="panel-body">
       <el-card class="box-card">
         <div slot="header" class="clearfix">
+          <i class="fa fa-newspaper-o fa-lg" aria-hidden="true" ></i>
           <span>公告</span>
           <el-button style="float: right;" @click.stop="on_refresh" size="small">
             <i class="fa fa-refresh"></i>
           </el-button>
         </div>
-        <div v-for="o in 4" :key="o" class="text item">
-          {{'列表内容 ' + o }}
+        <div v-for="o in 10" :key="o" class="text item">
+          {{'公告内容 ' + o }}
         </div>
+        <bottom-tool-bar>
+          <div slot="page" class="pagination">
+            <el-pagination
+              @current-change="handletipChange"
+              :current-page="tipcurrentPage"
+              :page-size="10"
+              layout="total, prev, pager, next"
+              :total="tiptotal">
+            </el-pagination>
+          </div>
+        </bottom-tool-bar>
       </el-card>
+
       <el-card class="box-card" style="margin-top: 40px">
         <div slot="header" class="clearfix">
+          <i class="fa fa-newspaper-o fa-lg" aria-hidden="true" ></i>
           <span>新闻</span>
           <el-button style="float: right;" @click.stop="on_refresh" size="small">
             <i class="fa fa-refresh"></i>
           </el-button>
         </div>
-        <div v-for="o in 4" :key="o" class="text item">
-          {{'列表内容 ' + o }}
+        <div v-for="o in 10" :key="o" class="text item">
+
+          {{'新闻内容 ' + o }}
         </div>
+        <bottom-tool-bar>
+          <div slot="page" class="pagination">
+            <el-pagination
+              @current-change="handlenewsChange"
+              :current-page="newscurrentPage"
+              :page-size="10"
+              layout="total, prev, pager, next"
+              :total="newstotal">
+            </el-pagination>
+          </div>
+        </bottom-tool-bar>
       </el-card>
+
     </div>
   </div>
 </template>
 <script type="text/javascript">
-  import {panelTitle} from 'components'
-
+  import {panelTitle, bottomToolBar} from 'components'
+  import axios from 'axios'
   export default{
     data(){
       return {
-
+        tipcurrentPage:1,
+        newscurrentPage:1,
+        tiptotal:15,
+        newstotal:15,
+        tiplength:10,
+        newslength:10
       }
     },
     components: {
-      panelTitle
+      panelTitle,
+      bottomToolBar
     },
     methods:{
       on_refresh(){
 
+      },
+      get_news(){
+
+      },
+      get_tips(){
+
+      },
+      handletipChange(val){
+        this.tipcurrentPage=val
+        this.get_tips()
+      },
+      handlenewsChange(val){
+        this.newscurrentPage=val
+        this.get_news()
       }
     }
   }
@@ -67,5 +114,9 @@
 
   .box-card {
     width: 100%;
+  }
+  .pagination{
+
+    margin: 0 auto;
   }
 </style>
